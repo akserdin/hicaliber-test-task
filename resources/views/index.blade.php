859a63b293871c301c7f8a9fe8ed9a2393f3e25b
Dmytro Zolotar <dmytrozolotar@gmail.com>
2017-09-07 00:48:36 +0300

    Clean template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -68,15 +68,7 @@
 
         <div v-if="! loading" class="row">
             <div v-for="item in results" class="col-lg-4">
-                <div class="well">
-                    <h3 v-text="item.name"></h3>
-                    <h4>@{{ item.price | currency('$', 0) }}</h4>
-
-                    <p>Bedrooms: <span v-text="item.bedrooms"></span></p>
-                    <p>Bathrooms: <span v-text="item.bathrooms"></span></p>
-                    <p>Garages: <span v-text="item.garages"></span></p>
-                    <p>Storeys: <span v-text="item.storeys"></span></p>
-                </div>
+                <item :item="item"></item>
             </div>
         </div>
     </div>
