2f1b211b28abaccef2cecf27aba713520af661c0
Dmytro Zolotar <dmytrozolotar@gmail.com>
2017-09-07 00:47:34 +0300

    Extract item to component

---
+++ resources/assets/js/components/Item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="well">
+        <h3 v-text="item.name"></h3>
+        <h4>{{ item.price | currency('$', 0) }}</h4>
+
+        <p>Bedrooms: <span v-text="item.bedrooms"></span></p>
+        <p>Bathrooms: <span v-text="item.bathrooms"></span></p>
+        <p>Garages: <span v-text="item.garages"></span></p>
+        <p>Storeys: <span v-text="item.storeys"></span></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['item']
+    }
+</script>
