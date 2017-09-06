a4d0a4964f85b1981932fed5b28ebe7fc8aba9b0
Dmytro Zolotar <dmytrozolotar@gmail.com>
2017-09-07 00:40:35 +0300

    Add search and display feature

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,14 +2,76 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+const availableFilters = [
+    {display: 'Bedrooms', name: 'bedrooms', min: 1, max: 5, value: null},
+    {display: 'Bathrooms', name: 'bathrooms', min: 1, max: 3, value: null},
+    {display: 'Storeys', name: 'storeys', min: 1, max: 2, value: null},
+    {display: 'Garages', name: 'garages', min: 1, max: 2, value: null},
+    {display: 'Price, min', name: 'price_min', min: 263604, max: 521951, value: null},
+    {display: 'Price, max', name: 'price_max', min: 263604, max: 521951, value: null}
+];
+
+import Vue2Filters from 'vue2-filters';
+
+Vue.use(Vue2Filters);
+
 new Vue({
     el: '#app',
 
     data: {
-        loading: false
+        loading: false,
+        results: [],
+
+        search: '',
+        availableFilters: []
+    },
+
+    mounted() {
+        this.availableFilters = availableFilters;
     },
 
     methods: {
-        //
+        showMessage(text) {
+            alert(text);
+        },
+
+        getAppliedFilters() {
+            let vm = this;
+            let appliedFilters = {};
+
+            vm.availableFilters.forEach(function(f) {
+                if (f.value !== null) {
+                    appliedFilters[f.name] = f.value;
+                }
+            });
+
+            if (vm.search.length > 1) {
+                appliedFilters.name = vm.search;
+            }
+
+            return appliedFilters;
+        },
+
+        request() {
+            let vm = this;
+
+            vm.loading = true;
+
+            axios.get('/properties', {params: vm.getAppliedFilters()})
+                .then(vm.handleResponse)
+                .catch(function(err) {});
+        },
+
+        handleResponse(res) {
+            let vm = this;
+
+            vm.results = res.data;
+
+            if (vm.results.length === 0) {
+                vm.showMessage('No properties found!');
+            }
+
+            vm.loading = false;
+        }
     }
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,62 @@
         <h1>Search for a property <small>Test task</small></h1>
     </div>
 
-    <div id="app"></div>
+    <div id="app">
+        <div v-if="loading">
+            <p class="text-center lead">Loading...</p>
+        </div>
+
+        <form>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1 col-xs-12">
+                    <div class="form-group">
+                        <input type="text" class="form-control input-lg" v-model.trim="search" :disabled="loading" placeholder="Type property name...">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <template v-for="filter in availableFilters">
+                    <div class="col-lg-3" v-if="filter.name === 'price_min' || filter.name === 'price_max'" :class="{ 'col-lg-offset-3': filter.name === 'price_min'}">
+                        <div class="form-group">
+                            <label v-text="filter.value ? filter.display + ' $' + filter.value : filter.display"></label>
+                            <input type="range" :min="filter.min" :max="filter.max" class="form-control input-lg" v-model="filter.value" :disabled="loading">
+                        </div>
+                    </div>
+
+                    <div v-else class="col-lg-3">
+                        <div class="form-group">
+                            <label v-text="filter.display"></label>
+                            <select class="form-control input-lg" v-model="filter.value" :disabled="loading">
+                                <option :value="null">Any</option>
+                                <option v-for="num in filter.max" v-text="num"></option>
+                            </select>
+                        </div>
+                    </div>
+                </template>
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-success btn-lg center-block" @click.prevent="request" :disabled="loading">Search</button>
+            </div>
+        </form>
+
+        <p v-if="results.length > 0 && ! loading">Found: <span v-text="results.length"></span></p>
+
+        <div v-if="! loading" class="row">
+            <div v-for="item in results" class="col-lg-4">
+                <div class="well">
+                    <h3 v-text="item.name"></h3>
+                    <h4>@{{ item.price | currency('$', 0) }}</h4>
+
+                    <p>Bedrooms: <span v-text="item.bedrooms"></span></p>
+                    <p>Bathrooms: <span v-text="item.bathrooms"></span></p>
+                    <p>Garages: <span v-text="item.garages"></span></p>
+                    <p>Storeys: <span v-text="item.storeys"></span></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script src="/js/app.js"></script>
