c1889c6ba0b27a7eb0f688223461a58fbc45e112
Dmytro Zolotar <dmytrozolotar@gmail.com>
2017-09-06 23:55:19 +0300

    Add seeder for properties

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $data = array_map('str_getcsv', file(storage_path('app/property-data.csv')));
+
+        foreach ($data as $index => $row) {
+            if ($index === 0) {
+                continue;
+            }
+
+            \App\Property::create([
+                'name' => $row[0],
+                'price' => $row[1],
+                'bedrooms' => $row[2],
+                'bathrooms' => $row[3],
+                'storeys' => $row[4],
+                'garages' => $row[5]
+            ]);
+        }
     }
 }
